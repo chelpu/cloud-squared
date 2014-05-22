ae0f332335c765bf80f232f69ecdce9d76afbcd3
Chelsea <chelpu@umich.edu>
2014-05-21 22:05:18 -0700

    took out sc stuff

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ import soundcloud
 
 app = Flask(__name__)
 
-client = soundcloud.Client(client_id='2a48aabb635303b6544ac9482529822a')
-tracks = client.get('/tracks', q='hello')
-track = tracks[0]
-stream_url = client.get(track.stream_url, allow_redirects=False)
-
-print stream_url.location
+#client = soundcloud.Client(client_id='2a48aabb635303b6544ac9482529822a')
+#tracks = client.get('/tracks', q='hello')
+#track = tracks[0]
+#stream_url = client.get(track.stream_url, allow_redirects=False)
+#
+#print stream_url.location
 
 @app.route("/", methods=['GET', 'POST'])
 def disp():
