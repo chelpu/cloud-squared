f88b01d8feca087d1df5344e886321bb0d1240e8
Chelsea Pugh <cpugh@cpugh.gateway.2wire.net>
2014-05-21 21:36:51 -0700

    add some soundcloud

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import soundcloud
 
 app = Flask(__name__)
 
+client = soundcloud.Client(client_id='2a48aabb635303b6544ac9482529822a')
+tracks = client.get('/tracks', q='hello')
+track = tracks[0]
+stream_url = client.get(track.stream_url, allow_redirects=False)
+
+print stream_url.location
+
 @app.route("/", methods=['GET', 'POST'])
-def hello():
+def disp():
 	resp = twilio.twiml.Response()
-	resp.message("Hello Buddy")
+	resp.message(stream_url.location)
 	return str(resp)
 
 if __name__=="__main__":
