136d9da85c9992d382252aabf0e6070d29662bde
Chelsea <chelpu@umich.edu>
2014-05-22 09:15:37 -0700

    took out sc stuff

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import os
 import soundcloud
 
 app = Flask(__name__)
@@ -18,5 +19,6 @@ def disp():
 	return str(resp)
 
 if __name__=="__main__":
-	app.run(debug=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
 
