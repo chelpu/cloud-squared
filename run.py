4e1bbc6d40be2b604c2a5a8b05f1ab79fd3cfba2
Chelsea <chelpu@umich.edu>
2014-05-22 12:28:35 -0700

    adding some call stuff

--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+from twilio.rest import TwilioRestClient
 import os
 import soundcloud
 
 app = Flask(__name__)
 
+	
+account_sid = "AC5116d5d4df9f61ceae2f0732e1ea9f1b"
+auth_token = "a7628c89db064134c18bec81b380722b"
+clientTwil = TwilioRestClient(account_sid, auth_token)
 
+playURL = ""
 
 @app.route("/", methods=['GET', 'POST'])
 def run():
@@ -22,9 +28,20 @@ def run():
 
 	resp = twilio.twiml.Response()
 	resp.message(titleAndArtist)
-	resp.play(stream_url.location)
-	#resp.message(body)
+	playURL = stream_url.location
+
+	# make a call to the client who texted in
+	call = clientTwil.calls.create(to=request.values.get('From', None),
+								   from_="+16162882901",
+								   url="http://cloud-squared.herokuapp.com/play")
 	return str(resp)
+
+@app.route("/play", methods=['GET', 'POST'])
+def play():
+	resp = twilio.twiml.Response()
+	resp.play(playURL)
+	return str(resp)
+
 
 if __name__=="__main__":
 	app.run(debug=True)
