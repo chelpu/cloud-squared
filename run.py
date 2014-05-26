6fcef048ab53bd66a85b57acb398eb88697a0386
Chelsea <chelpu@umich.edu>
2014-05-26 15:54:46 -0700

    sending url

--- run.py
+++ run.py
@@ -17,7 +17,6 @@ def getTrack(query, client, i, nOrC):
 	track = tracks[i]
 
 	if nOrC == "n":
-		print "NEWWWWW"
 		i = i+1
 		while (track.sharing.startswith("pri") or not track.streamable) and i < tracks.count:
 			track = tracks[i]
@@ -30,7 +29,6 @@ client = soundcloud.Client(client_id=secrets.get("soundcloud", "client_id"))
 
 @app.route("/text", methods=['GET', 'POST'])
 def run():
-	print "IN TEXT"
 	body = ""
 	option = request.args.get('opt', '')
 	if option == "4":
@@ -43,29 +41,26 @@ def run():
 	stream_url = client.get(track.stream_url, allow_redirects=False)
 
 	titleAndArtist = track.title + ' - ' + track.user["username"]
-	print titleAndArtist
-	print stream_url.location
-
+	songURL = track.permalink_url
 	resp = twilio.twiml.Response()
 	resp.message(titleAndArtist)
 	playURL = stream_url.location
 
 	encoded = urllib.quote_plus(playURL)
 	encodedBody = urllib.quote_plus(body)
-
+	encodedSongUrl = urllib.quote_plus(songURL)
 	cur = urllib.quote_plus(str(i))
 
-	# make a call to the client who texted in
+	# Make a call to the client who texted in
 	call = clientTwil.calls.create(to=request.values.get('From', None),
 								   from_="+16162882901",
-								   url="http://cloud-squared.herokuapp.com/play?query=" + encodedBody + "&sound=" + encoded + "&opt=0&cur=" + cur)
+								   url="http://cloud-squared.herokuapp.com/play?query=" + encodedBody + "&sound=" + encoded + "&opt=0&cur=" + cur + "&url=" + encodedSongUrl)
 	return str(resp)
 
 @app.route("/call", methods=['GET', 'POST'])
 def call():
 	resp = twilio.twiml.Response()
-	resp.say("What would you like to search for?")
-	resp.record(maxLength="10", transcribe="true", finishOnKey="#", transcribeCallback="http://cloud-squared.herokuapp.com/text?option=4")
+	resp.say("Text this number with a search term to hear a song")
 	return str(resp)
 
 @app.route("/play", methods=['GET', 'POST'])
@@ -74,14 +69,17 @@ def play():
 	cur = request.args.get('cur', '')
 	sound = request.args.get('sound', '')
 	query = request.args.get('query', '')
+	songURL = request.args.get('url', '')
+
 	encoded = urllib.quote_plus(query)
+	encodedSongUrl = urllib.quote_plus(songURL)
 	cur = urllib.quote_plus(cur)
 				
 	resp = twilio.twiml.Response()
 	
 	resp.say("Press 1 to skip to a different song")
 	resp.say("Press 2 to receive a link to this song")
-	with resp.gather(numDigits=1, action="/handle-key?query=" + encoded + "&cur=" + cur, method="POST") as g:
+	with resp.gather(numDigits=1, action="/handle-key?query=" + encoded + "&cur=" + cur + "&url=" + encodedSongUrl, method="POST") as g:
 		g.play(sound)
 	return str(resp)
 
@@ -94,11 +92,11 @@ def handle_key():
 
 	cur = request.args.get('cur', '')
 	query = request.args.get('query', '')
+	songURL = request.args.get('url', '')
 	encoded = urllib.quote_plus(query)
 
 	# Get the digit pressed by the user
 	if digit_pressed == "1":
-		print "CUR IN HK: ", cur
 		d = getTrack(query, client, int(cur), "n")
 		track = d["track"]
 		i = d["i"]
@@ -106,6 +104,7 @@ def handle_key():
 		message = clientTwil.messages.create(to=to, from_="+16162882901",
                                      		 body=titleAndArtist)
 		cur = urllib.quote_plus(str(i))
+
 		# Get url to send back to play
 		stream_url = client.get(track.stream_url, allow_redirects=False)
 		playURL = stream_url.location
@@ -123,11 +122,9 @@ def handle_key():
 		print track.purchase_url
 		print "DOWNLOADABLE? ", track.downloadable
 		if track.permalink_url != "":
-			#resp.message(track.download_url)
 			message = clientTwil.messages.create(to=to, from_="+16162882901",
-                                     body=track.permalink_url)
+                                     body=songURL)
 		else:
-			#resp.message("Sorry, download link unavailable")
 			message = clientTwil.messages.create(to=to, from_="+16162882901",
                                      body="Sorry, link unavailable")
 		return str(resp)
@@ -136,6 +133,6 @@ def handle_key():
 	else:
 		return redirect("/play")
 
-if __name__=="__main__":
+if __name__ == "__main__":
 	app.run(debug=True)
 
