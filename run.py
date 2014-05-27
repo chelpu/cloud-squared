52fcda4187b5f4dd24979a1acd1bde36aef1dad1
Chelsea <chelpu@umich.edu>
2014-05-26 17:39:32 -0700

    dont hang up when sending url?

--- run.py
+++ run.py
@@ -127,6 +127,8 @@ def handle_key():
 		else:
 			message = clientTwil.messages.create(to=to, from_="+16162882901",
                                      body="Sorry, link unavailable")
+		with resp.gather(numDigits=1, action="/handle-key?query=" + encoded + "&cur=" + cur, method="POST") as g:
+			g.play(playURL)
 		return str(resp)
  
 	# If the caller pressed anything but 1, redirect them to the homepage.
