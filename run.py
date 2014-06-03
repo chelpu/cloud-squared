bf9c0eab941708f16fa7d60286dc1b230408a73d
Chelsea <chelpu@umich.edu>
2014-06-03 13:32:44 -0700

    cleaning up

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ secrets.read('secrets.ini')
 
 app = Flask(__name__)
 
-def getTrack(query, client, i, nOrC):
-	tracks = client.get('/tracks', q=query)
+def get_track(query, client_sc, i, nOrC):
+	tracks = client_sc.get('/tracks', q=query)
 	track = tracks[i]
 
 	if nOrC == "n":
@@ -22,33 +22,34 @@ def getTrack(query, client, i, nOrC):
 			i = i+1
 	return (track, i)
 
-clientTwil = TwilioRestClient(secrets.get("twilio", "account_sid"), 
-							  secrets.get("twilio", "auth_token"))
-client = soundcloud.Client(client_id=secrets.get("soundcloud", "client_id"))
+client_twil = TwilioRestClient(secrets.get("twilio", "account_sid"), 
+							   secrets.get("twilio", "auth_token"))
+client_sc = soundcloud.Client(client_id=secrets.get("soundcloud", "client_id"))
 baseURL = "http://cloud-squared.herokuapp.com"
 
 @app.route("/text", methods=['GET', 'POST'])
 def run():
 	body = request.values.get('Body', None)
-	(track, i) = getTrack(body, client, 0, "n")
+	(track, i) = get_track(body, client_sc, 0, "n")
 	
-	stream_url = client.get(track.stream_url, allow_redirects=False)
+	stream_url = client_sc.get(track.stream_url, allow_redirects=False)
 
-	titleAndArtist = track.title + ' - ' + track.user["username"]
-	songURL = track.permalink_url
+	title_and_artist = track.title + ' - ' + track.user["username"]
+	song_url = track.permalink_url
+	play_url = stream_url.location
+
 	resp = twilio.twiml.Response()
-	resp.message(titleAndArtist)
-	playURL = stream_url.location
+	resp.message(title_and_artist)
 
-	encodedPlayURL = urllib.quote_plus(playURL)
-	encodedBody = urllib.quote_plus(body)
-	encodedSongUrl = urllib.quote_plus(songURL)
+	encoded_play_url = urllib.quote_plus(play_url)
+	encoded_query = urllib.quote_plus(body)
+	encoded_song_url = urllib.quote_plus(song_url)
 	cur = urllib.quote_plus(str(i))
 
 	# Make a call to the user who texted in
-	call = clientTwil.calls.create(to=request.values.get('From', None),
-								   from_="+16162882901",
-								   url=baseURL + "/play?query=" + encodedBody + "&sound=" + encodedPlayURL + "&cur=" + cur + "&url=" + encodedSongUrl)
+	call = client_twil.calls.create(to=request.values.get('From', None),
+									from_="+16162882901",
+									url=baseURL + "/play?query=" + encoded_query + "&sound=" + encoded_play_url + "&cur=" + cur + "&url=" + encoded_song_url)
 	return str(resp)
 
 @app.route("/call", methods=['GET', 'POST'])
@@ -62,81 +63,79 @@ def play():
 	cur = request.args.get('cur', '')
 	sound = request.args.get('sound', '')
 	query = request.args.get('query', '')
-	songURL = request.args.get('url', '')
+	song_url = request.args.get('url', '')
 
-	encodedQuery = urllib.quote_plus(query)
-	encodedSongUrl = urllib.quote_plus(songURL)
+	encoded_query = urllib.quote_plus(query)
+	encoded_song_url = urllib.quote_plus(song_url)
+	encoded_sound = urllib.quote_plus(sound)
 	cur = urllib.quote_plus(cur)
-	encodedSound = urllib.quote_plus(sound)
 				
 	resp = twilio.twiml.Response()
 	
 	resp.say("Press 1 to skip to a different song")
 	resp.say("Press 2 to receive a link to this song")
-	with resp.gather(numDigits=1, action="/handle-key?query=" + encodedQuery + "&cur=" + cur + "&url=" + encodedSongUrl + "&sound=" + encodedSound, method="POST") as g:
+	with resp.gather(numDigits=1, action="/handle-key?query=" + encoded_query + "&cur=" + cur + "&url=" + encoded_song_url + "&sound=" + encoded_sound, method="POST") as g:
 		g.play(sound)
 	return str(resp)
 
 @app.route("/handle-key", methods=['GET', 'POST'])
 def handle_key(): 
-
 	resp = twilio.twiml.Response()
 	digit_pressed = request.values.get('Digits', None)
 	to = request.values.get('To', None)
 
 	cur = request.args.get('cur', '')
 	query = request.args.get('query', '')
-	songURL = request.args.get('url', '')
+	song_url = request.args.get('url', '')
 	sound = request.args.get('sound', '')
 	
-	encodedSongUrl = urllib.quote_plus(songURL)
-	encoded = urllib.quote_plus(query)
-	encodedSound = urllib.quote_plus(sound)
+	encoded_song_url = urllib.quote_plus(song_url)
+	encoded_query = urllib.quote_plus(query)
+	encoded_sound = urllib.quote_plus(sound)
 
 	# Get the digit pressed by the user
 	if digit_pressed == "1":
-		(track, i) = getTrack(query, client, int(cur), "n")
+		(track, i) = get_track(query, client_sc, int(cur), "n")
 		
-		titleAndArtist = track.title + ' - ' + track.user["username"]
-		message = clientTwil.messages.create(to=to, from_="+16162882901",
-                                     		 body=titleAndArtist)
+		title_and_artist = track.title + ' - ' + track.user["username"]
+		message = client_twil.messages.create(to=to, from_="+16162882901",
+                                     		 body=title_and_artist)
 		cur = urllib.quote_plus(str(i))
 
-		songURL = track.permalink_url
-		encodedSongUrl = urllib.quote_plus(songURL)
+		song_url = track.permalink_url
+		encoded_song_url = urllib.quote_plus(song_url)
 
 		# Get url to send back to play
-		stream_url = client.get(track.stream_url, allow_redirects=False)
-		playURL = stream_url.location
-		encodedURL = urllib.quote_plus(playURL)
+		stream_url = client_sc.get(track.stream_url, allow_redirects=False)
+		play_url = stream_url.location
+
+		encoded_url = urllib.quote_plus(play_url)
 		cur = urllib.quote_plus(cur)
 
-		songURL = stream_url
-
-		with resp.gather(numDigits=1, action="/handle-key?query=" + encoded + "&cur=" + cur + "&url=" + encodedSongUrl + "&sound=" + encodedURL, method="POST") as g:
-			g.play(playURL)
+		with resp.gather(numDigits=1, action="/handle-key?query=" + encoded_query + "&cur=" + cur + "&url=" + encoded_song_url + "&sound=" + encoded_url, method="POST") as g:
+			g.play(play_url)
 
 		return str(resp)
 
 	elif digit_pressed == "2":
-		(track, i) = getTrack(query, client, int(cur), "c")
+		(track, i) = get_track(query, client_sc, int(cur), "c")
 		
 		if track.permalink_url != "":
-			message = clientTwil.messages.create(to=to, from_="+16162882901",
-                                     			 body=songURL)
+			message = client_twil.messages.create(to=to, from_="+16162882901",
+                                     			 body=song_url)
 		else:
-			message = clientTwil.messages.create(to=to, from_="+16162882901",
+			message = client_twil.messages.create(to=to, from_="+16162882901",
                                      			 body="Sorry, link unavailable")
 
 		cur = urllib.quote_plus(cur)
-		with resp.gather(numDigits=1, action="/handle-key?query=" + encoded + "&cur=" + cur + "&url=" + encodedSongUrl + "&sound=" + sound, method="POST") as g:
+		with resp.gather(numDigits=1, action="/handle-key?query=" + encoded_query + "&cur=" + cur + "&url=" + encoded_song_url + "&sound=" + sound, method="POST") as g:
 			g.play(sound)
 		return str(resp)
  
 	# If the caller pressed anything but 1, redirect them to the homepage.
 	else:
 		cur = urllib.quote_plus(cur)
-		return redirect("/play?query=" + encoded + "&sound=" + encodedSound + "&cur=" + cur + "&url=" + encodedSongUrl)
+		return redirect("/play?query=" + encoded_query + "&sound=" + encoded_sound + "&cur=" + cur + "&url=" + encoded_song_url)
 
 if __name__ == "__main__":
 	app.run(debug=True)
